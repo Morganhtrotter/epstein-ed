f77071e8f03c8573c0ce256c43eedf4101c472a1
Morgan Trotter <mhtrotte@ucsc.edu>
2021-05-16 16:17:45 -0700

    moved files

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import { HashRouter } from 'react-router-dom';
 import './App.css';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore.js';
-import firebase from "firebase";
 
 const store = ConfigureStore();
 
 class App extends Component {
 
 	render() {
-		const firebaseApp = firebase.apps[0];
 
 		return (
 			<Provider store={store}>
